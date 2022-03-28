b3ed9af0b4220fa4be5d018342abf9a1a82ebbc3
wenbin <wenbin.shi@gmail.com>
2022-03-28 23:35:59 +0800

    add binary

--- 4_four_nodes/build_for_docker.sh
+++
@@ -1,8 +0,0 @@
-# update the resource limit in docker Desktop app
-# OS: mac
-# binary: for ubuntu platform
-
-
-rm -rf $GOPATH/bin/teleport_ubuntu
-
-docker run -it -v $GOPATH:/root/go -v $GOPATH/pkg:/go/pkg arm64v8/golang /bin/bash -c "cd /root/go/src/github.com/teleport-network/teleport && make install && mv /go/bin/teleport /root/go/bin/teleport_ubuntu"
--- 4_four_nodes/clear.sh
+++
@@ -1,3 +0,0 @@
-docker rm -f peer0 peer1 peer2 peer3
-
-rm -rf ~/teleport_testnet/
